b3111bede60f5f3d11d3bc2cfb2fd915e4e78049
Carlito-Tagarro <carlitotagarro0@gmail.com>
2025-08-02 17:18:59 +0800

    database

---
+++ inventory_system (1).sql
@@ -0,0 +1,345 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 01, 2025 at 10:57 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventory_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brochures`
+--
+
+CREATE TABLE `brochures` (
+  `brochure_id` int(11) NOT NULL,
+  `brochure_name` varchar(100) NOT NULL,
+  `quantity` int(4) NOT NULL,
+  `total_brochure` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `brochures`
+--
+
+INSERT INTO `brochures` (`brochure_id`, `brochure_name`, `quantity`, `total_brochure`) VALUES
+(8, 'Trifold', 1, 0),
+(9, 'CSB', 1, 0),
+(10, 'AWS', 1, 0),
+(11, 'Swift', 1, 0),
+(12, 'IC3', 1, 0),
+(13, 'QuickBooks', 1, 0),
+(14, 'Autodesk', 1, 0),
+(15, 'Microsoft Certification', 1, 0),
+(16, 'The Value', 1, 0),
+(17, 'ESB', 1, 0),
+(18, 'Benefits of Microsoft', 1, 0),
+(19, 'One Pager', 1, 0),
+(20, 'Copilot', 1, 0),
+(21, 'Versant', 1, 0),
+(22, 'PMI', 1, 0),
+(23, 'Value of Certification', 1, 0),
+(24, 'Cisco', 1, 0),
+(25, 'Adobe', 1, 0),
+(26, 'Meta', 1, 0),
+(27, 'ITS', 1, 0),
+(28, 'Unity', 1, 0),
+(29, 'CCS', 1, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_form`
+--
+
+CREATE TABLE `event_form` (
+  `event_form_id` int(11) NOT NULL,
+  `event_name` varchar(100) NOT NULL,
+  `event_title` varchar(100) NOT NULL,
+  `event_date` date NOT NULL,
+  `date_time_ingress` datetime NOT NULL,
+  `date_time_egress` datetime NOT NULL,
+  `place` varchar(100) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `sponsorship_budg` enum('Free','Sponsorship','','') DEFAULT NULL,
+  `target_audience` text DEFAULT NULL,
+  `number_audience` int(11) NOT NULL,
+  `set_up` enum('Yes','No','','') DEFAULT NULL,
+  `booth_size` text DEFAULT NULL,
+  `booth_inclusion` text DEFAULT NULL,
+  `number_tables` int(3) DEFAULT NULL,
+  `number_chairs` int(3) DEFAULT NULL,
+  `speaking_slot` varchar(255) DEFAULT NULL,
+  `date_time` datetime NOT NULL,
+  `program_target` varchar(255) DEFAULT NULL,
+  `technical_team` enum('Yes','No','','') NOT NULL,
+  `trainer_needed` enum('Yes','No','','') NOT NULL,
+  `ready_to_use` text DEFAULT NULL,
+  `provide_materials` enum('Yes','No','','') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `request_status` enum('Pending','Approved','Decline','') NOT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_form_history`
+--
+
+CREATE TABLE `event_form_history` (
+  `event_form_id` int(11) NOT NULL,
+  `event_name` varchar(255) DEFAULT NULL,
+  `event_title` varchar(255) DEFAULT NULL,
+  `event_date` date DEFAULT NULL,
+  `sender_email` varchar(255) DEFAULT NULL,
+  `date_time_ingress` datetime DEFAULT NULL,
+  `date_time_egress` datetime DEFAULT NULL,
+  `place` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `sponsorship_budget` decimal(10,2) DEFAULT NULL,
+  `target_audience` varchar(255) DEFAULT NULL,
+  `number_audience` int(11) DEFAULT NULL,
+  `set_up` varchar(255) DEFAULT NULL,
+  `booth_size` varchar(255) DEFAULT NULL,
+  `booth_inclusion` varchar(255) DEFAULT NULL,
+  `number_tables` int(11) DEFAULT NULL,
+  `number_chairs` int(11) DEFAULT NULL,
+  `speaking_slot` varchar(255) DEFAULT NULL,
+  `date_time` datetime DEFAULT NULL,
+  `program_target` varchar(255) DEFAULT NULL,
+  `technical_team` varchar(255) DEFAULT NULL,
+  `trainer_needed` varchar(255) DEFAULT NULL,
+  `ready_to_use` varchar(255) DEFAULT NULL,
+  `provide_materials` varchar(255) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `request_status` varchar(50) DEFAULT NULL,
+  `processed_at` datetime DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `marketing_materials`
+--
+
+CREATE TABLE `marketing_materials` (
+  `material_id` int(11) NOT NULL,
+  `material_name` varchar(100) NOT NULL,
+  `quantity` int(4) NOT NULL,
+  `others` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `marketing_materials`
+--
+
+INSERT INTO `marketing_materials` (`material_id`, `material_name`, `quantity`, `others`) VALUES
+(5, 'Door Banner', 1, NULL),
+(6, 'Door Banner Stand', 1, NULL),
+(7, 'Back Drop', 1, NULL),
+(8, 'Back Drop Stand', 1, NULL),
+(9, 'Table Cloth', 1, NULL),
+(10, 'Brochure Rack', 1, NULL),
+(11, 'Acrylic Marketing Stand', 1, NULL),
+(12, 'Handy Props', 1, NULL),
+(13, 'Extension Cord', 1, NULL),
+(14, 'Camera', 1, NULL),
+(15, 'Tripod', 1, NULL),
+(16, 'Jars for Chocolates', 1, NULL),
+(17, 'BIR 2307 for hotel', 1, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `material_request_form`
+--
+
+CREATE TABLE `material_request_form` (
+  `material_request_id` int(11) NOT NULL,
+  `request_mats` int(11) NOT NULL,
+  `name_brochures` varchar(255) NOT NULL,
+  `brochure_quantity` int(11) NOT NULL,
+  `name_swag` varchar(255) NOT NULL,
+  `swag_quantity` int(11) NOT NULL,
+  `name_material` varchar(255) NOT NULL,
+  `material_quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `swags`
+--
+
+CREATE TABLE `swags` (
+  `swag_id` int(11) NOT NULL,
+  `swags_name` varchar(100) NOT NULL,
+  `quantity` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `swags`
+--
+
+INSERT INTO `swags` (`swag_id`, `swags_name`, `quantity`) VALUES
+(2, 'Audentes Lanyard', 1),
+(3, 'Microsoft Lanyard', 1),
+(4, 'Pins', 22),
+(5, 'T-shirt', 1),
+(6, 'Stickers', 1),
+(7, 'Ballpens', 1),
+(8, 'Chocolates', 1),
+(9, 'Note Pads', 1),
+(10, 'Mouse Pads', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `user_type` enum('admin','trainer','','') DEFAULT NULL,
+  `position` varchar(100) NOT NULL,
+  `verification_code` varchar(10) NOT NULL,
+  `verified` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `email`, `user_type`, `position`, `verification_code`, `verified`) VALUES
+(13, 'administration', '$2y$10$CP9GOla6e9kfDWiDpgDTsux.PJ0UpiXL2hBTudOpsGPAKeArFDr.W', 'carlitotagarro0@gmail.com', 'admin', '', '921859', 1),
+(14, 'user', '$2y$10$nZLjh4MA0DRpnCwQ7l4JTuXl6qwc6WQF28Zpt13eLpaamtPE7tPAW', 'carlitotagarro27@gmail.com', 'trainer', '', '290166', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brochures`
+--
+ALTER TABLE `brochures`
+  ADD PRIMARY KEY (`brochure_id`);
+
+--
+-- Indexes for table `event_form`
+--
+ALTER TABLE `event_form`
+  ADD PRIMARY KEY (`event_form_id`),
+  ADD KEY `fk_user_id` (`user_id`);
+
+--
+-- Indexes for table `event_form_history`
+--
+ALTER TABLE `event_form_history`
+  ADD PRIMARY KEY (`event_form_id`),
+  ADD KEY `fk_event_form_history_user_id` (`user_id`);
+
+--
+-- Indexes for table `marketing_materials`
+--
+ALTER TABLE `marketing_materials`
+  ADD PRIMARY KEY (`material_id`);
+
+--
+-- Indexes for table `material_request_form`
+--
+ALTER TABLE `material_request_form`
+  ADD PRIMARY KEY (`material_request_id`);
+
+--
+-- Indexes for table `swags`
+--
+ALTER TABLE `swags`
+  ADD PRIMARY KEY (`swag_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brochures`
+--
+ALTER TABLE `brochures`
+  MODIFY `brochure_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+
+--
+-- AUTO_INCREMENT for table `event_form`
+--
+ALTER TABLE `event_form`
+  MODIFY `event_form_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `marketing_materials`
+--
+ALTER TABLE `marketing_materials`
+  MODIFY `material_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `material_request_form`
+--
+ALTER TABLE `material_request_form`
+  MODIFY `material_request_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `swags`
+--
+ALTER TABLE `swags`
+  MODIFY `swag_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `event_form`
+--
+ALTER TABLE `event_form`
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `event_form_history`
+--
+ALTER TABLE `event_form_history`
+  ADD CONSTRAINT `fk_event_form_history_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
