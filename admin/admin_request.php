3cb083058731362bfb84feeaabe95d49ffb95961
Carlito-Tagarro <carlitotagarro0@gmail.com>
2025-08-25 16:46:05 +0800

    Fix admin request

--- admin/admin_request.php
+++ admin/admin_request.php
@@ -107,6 +107,7 @@ DISCONNECTIVITY($connection);
             <a href="swags_inventory.php">Swags Inventory</a>
         </div>
     </nav>
+      <h4>Note: Click <span style="color:#b91c1c;">View Details</span> to Download the PDF Format of the Request Form</h4>
     <div class="tables-flex">
         <div class="table-container">
             <h2>Event Requests</h2>
@@ -145,7 +146,6 @@ DISCONNECTIVITY($connection);
         </div>
         <div class="table-container">
             <h2>Processed Requests History</h2>
-            <h4>Note: Click <span style="color:crimson;">View Details</span> to Download the PDF Format of the Request Form</h4>
             <table>
                 <thead>
                     <tr>
