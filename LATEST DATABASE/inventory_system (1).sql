6143801d8efa4ce9a5743c42b39574fd44787dc4
Carlito-Tagarro <carlitotagarro0@gmail.com>
2025-09-08 16:58:34 +0800

    LATEST DB 08/09/2025

--- LATEST DATABASE/inventory_system (1).sql
+++ LATEST DATABASE/inventory_system (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 05, 2025 at 10:23 AM
+-- Generation Time: Sep 08, 2025 at 10:57 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -76,6 +76,8 @@ CREATE TABLE `event_form` (
   `event_name` varchar(100) NOT NULL,
   `event_title` varchar(100) NOT NULL,
   `event_date` varchar(50) NOT NULL,
+  `contact_person` varchar(50) NOT NULL,
+  `contact_number` varchar(20) NOT NULL,
   `event_duration` varchar(100) NOT NULL,
   `date_time_ingress` datetime NOT NULL,
   `date_time_egress` datetime NOT NULL,
@@ -86,6 +88,7 @@ CREATE TABLE `event_form` (
   `amount` int(11) DEFAULT NULL,
   `target_audience` text DEFAULT NULL,
   `number_audience` text NOT NULL,
+  `other_attendee` text NOT NULL,
   `set_up` enum('Yes','No','','') DEFAULT NULL,
   `booth_size` text DEFAULT NULL,
   `booth_inclusion` text DEFAULT NULL,
@@ -119,6 +122,8 @@ CREATE TABLE `event_form_history` (
   `event_name` varchar(255) DEFAULT NULL,
   `event_title` varchar(255) DEFAULT NULL,
   `event_date` varchar(100) DEFAULT NULL,
+  `contact_person` varchar(50) NOT NULL,
+  `contact_number` varchar(20) NOT NULL,
   `event_duration` varchar(100) NOT NULL,
   `sender_email` varchar(255) DEFAULT NULL,
   `date_time_ingress` datetime DEFAULT NULL,
@@ -130,6 +135,7 @@ CREATE TABLE `event_form_history` (
   `amount` int(11) DEFAULT NULL,
   `target_audience` varchar(255) DEFAULT NULL,
   `number_audience` varchar(255) DEFAULT NULL,
+  `other_attendee` varchar(255) NOT NULL,
   `set_up` enum('Yes','No','','') DEFAULT NULL,
   `booth_size` varchar(255) DEFAULT NULL,
   `booth_inclusion` varchar(255) DEFAULT NULL,
@@ -157,8 +163,10 @@ CREATE TABLE `event_form_history` (
 -- Dumping data for table `event_form_history`
 --
 
-INSERT INTO `event_form_history` (`event_form_id`, `event_name`, `event_title`, `event_date`, `event_duration`, `sender_email`, `date_time_ingress`, `date_time_egress`, `claiming_id`, `place`, `location`, `sponsorship_budg`, `amount`, `target_audience`, `number_audience`, `set_up`, `booth_size`, `booth_inclusion`, `number_tables`, `number_chairs`, `speaking_slot`, `speaker_name`, `date_time`, `duration`, `topic`, `technical_team`, `technical_task`, `trainer_needed`, `trainer_task`, `provide_materials`, `requested_by`, `created_at`, `request_status`, `processed_at`, `user_id`, `request_mats`) VALUES
-(83, 'VCT 2025', 'TESTING', 'October 1-5, 2025', '5', 'carlitotagarro27@gmail.com', '2025-10-01 17:13:00', '2025-10-05 05:13:00', 'October 8, 2025 | 6:00 PM – 11:00 PM or October 9, 2025 | 7:00 AM\r\n Meeting Room 2 – Registration Counter', 'Kahit saan dian sa Etivac', 'Etivac', 'Sponsorship', 100000, 'Mga Valorant Players', '1 million playas', '', '4x4', 'Submitted', 4, 5, 'Yes', 'Mr. Francis Xavier Hernandez', '2025-10-01 05:12:00', '10 minutes', 'How to move like a Radiant?', 'Yes', 'iassist ang hindi marunong gumamit ng mga device', 'Yes', 'Mag train ng mahihinang nilalang', 'No', 'Carlito R. Tagarro', '2025-09-05 16:16:44', 'Approved', '2025-09-05 16:17:53', 17, 0);
+INSERT INTO `event_form_history` (`event_form_id`, `event_name`, `event_title`, `event_date`, `contact_person`, `contact_number`, `event_duration`, `sender_email`, `date_time_ingress`, `date_time_egress`, `claiming_id`, `place`, `location`, `sponsorship_budg`, `amount`, `target_audience`, `number_audience`, `other_attendee`, `set_up`, `booth_size`, `booth_inclusion`, `number_tables`, `number_chairs`, `speaking_slot`, `speaker_name`, `date_time`, `duration`, `topic`, `technical_team`, `technical_task`, `trainer_needed`, `trainer_task`, `provide_materials`, `requested_by`, `created_at`, `request_status`, `processed_at`, `user_id`, `request_mats`) VALUES
+(94, '2025 PASUC Midterm Convention and General Assembly', 'Forging the Future: Empowering SUCs through Innovation,   Sustainability, and Excellence', 'September 9-11, 2025', '', '', '3 days', 'carlitotagarro27@gmail.com', '2025-09-09 01:48:00', '2025-09-11 13:48:00', 'July 8, 2025 | 6:00 PM – 11:00 PM or July 9, 2025 | 7:00 AM\r\n Meeting Room 2 – Registration Counter', 'Booth 24 &amp; 37   SMX Convention Center - Function Rooms 4 and 5', 'SMX Convention Center Manila, Pasay City', 'Sponsorship', 55000, 'SUC Presidents, Vice Presidents, Chancellors, Directors, \r\n Deans, faculty members, non-teaching staff, and key  \r\n government \r\n partners—are expected to attend.', '1,000 (113 State Universities and Colleges / 60 satellite campuses', '', 'Yes', '2.50m x 3.0m x 2.48m', 'Tables and Chairs', 2, 4, 'Yes', 'Mr. Francis Xavier Hernandez', '2025-09-11 13:30:00', '30 minutes', 'Ako', 'Yes', 'Help Assist with Technical Difficulties', 'Yes', 'Help Train People', 'Yes', '', '2025-09-08 13:56:13', 'Declined', '2025-09-08 13:58:01', 17, 325),
+(95, 'Testing Contact Person', 'Testing', 'September 1-5, 2025', 'Mr. Clingy Miggy', '', '5 days', 'carlitotagarro27@gmail.com', '2025-09-01 03:48:00', '2025-09-05 15:48:00', 'Basta', 'Hotel', 'Paranaque', 'Free', 445, 'Mga ewan', 'Madami boi', '', 'Yes', '4x4x4x4', 'Tables and Chairs', 5, 4, 'Yes', 'Mr. Francis Xavier Hernandez', '2025-09-04 15:49:00', '30 minutes', 'Ako', 'No', 'GG', 'No', 'GG', 'No', 'Mr.Tagarro', '2025-09-08 15:50:21', 'Declined', '2025-09-08 15:51:13', 17, 0),
+(96, 'Test  Other company attending and Contact Number', 'Testing', 'September 1-5, 2025', 'Mr. Clingy Miggy', '09405017962', '5 days', 'carlitotagarro27@gmail.com', '2025-09-01 16:51:00', '2025-09-05 16:51:00', 'TUMESTING LANG', 'TUMESTING LANG', 'Tumesting lang', 'Sponsorship', 55000, 'mga maangas', 'madami', 'SpaceX', 'No', 'Malaki Af', 'lahat', 5, 10, 'Yes', 'Mr. Francis Xavier Hernandez', '2025-09-05 14:30:00', '30 minutes', 'Ako', 'Yes', 'Need help for Technical Issues', 'Yes', 'Need help to assist explain the certifications', 'No', 'Mr. Carlito R. Tagarro', '2025-09-08 16:54:15', 'Approved', '2025-09-08 16:55:06', 17, 0);
 
 -- --------------------------------------------------------
 
@@ -206,9 +214,16 @@ CREATE TABLE `material_request_form` (
 --
 
 INSERT INTO `material_request_form` (`material_request_id`, `request_mats`, `name_brochures`, `brochure_quantity`, `name_swag`, `swag_quantity`, `name_material`, `material_quantity`) VALUES
-(322, 322, 'ALL PROGRAMS', 1, '', 0, '', 0),
-(323, 322, 'ADOBE', 1, '', 0, '', 0),
-(324, 322, 'AUTODESK', 1, '', 0, '', 0);
+(325, 325, 'ALL PROGRAMS', 1, '', 0, '', 0),
+(326, 325, 'ADOBE', 1, '', 0, '', 0),
+(327, 325, 'AUTODESK', 1, '', 0, '', 0),
+(328, 325, 'AWS', 2, '', 0, '', 0),
+(329, 325, '', 0, '', 0, 'MATERIALS 1', 1),
+(330, 325, '', 0, '', 0, 'MATERIALS 2', 1),
+(331, 325, '', 0, '', 0, 'MATERIALS 3', 1),
+(332, 325, '', 0, 'SWAGS 1', 1, '', 0),
+(333, 325, '', 0, 'SWAGS 2', 1, '', 0),
+(334, 325, '', 0, 'SWAGS 3', 1, '', 0);
 
 -- --------------------------------------------------------
 
@@ -225,13 +240,6 @@ CREATE TABLE `material_return_request` (
   `requested_at` datetime NOT NULL,
   `reviewed_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `material_return_request`
---
-
-INSERT INTO `material_return_request` (`request_id`, `event_id`, `user_id`, `items_json`, `status`, `requested_at`, `reviewed_at`) VALUES
-(13, 57, 17, '[{\"type\":\"Brochure\",\"name\":\"ALL PROGRAMS\",\"qty\":1},{\"type\":\"Brochure\",\"name\":\"ADOBE\",\"qty\":1},{\"type\":\"Brochure\",\"name\":\"AUTODESK\",\"qty\":1}]', 'Approved', '2025-08-20 15:14:56', '2025-08-20 15:15:31');
 
 -- --------------------------------------------------------
 
@@ -255,6 +263,55 @@ INSERT INTO `swags` (`swag_id`, `swags_name`, `quantity`) VALUES
 (3, 'SWAGS 3', 1),
 (4, 'SWAGS 4', 1),
 (5, 'SWAGS 5', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team`
+--
+
+CREATE TABLE `team` (
+  `attendee_id` int(11) NOT NULL,
+  `attendee_name` varchar(100) NOT NULL,
+  `event_form_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team_history`
+--
+
+CREATE TABLE `team_history` (
+  `team_history_id` int(11) NOT NULL,
+  `event_form_id` int(11) NOT NULL,
+  `attendee_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `team_history`
+--
+
+INSERT INTO `team_history` (`team_history_id`, `event_form_id`, `attendee_name`) VALUES
+(22, 94, 'Sir Francis'),
+(23, 94, 'Ms.Jaycel'),
+(24, 94, 'Sir Patrick'),
+(25, 94, 'Ms.Felice'),
+(26, 94, 'Sir MJ - Agile'),
+(27, 94, 'Ms.Sharlotte'),
+(28, 94, 'Sir Jerome'),
+(29, 94, 'Ms.Laine'),
+(30, 94, 'Driver ( Kuya Panoy )'),
+(31, 94, 'Ms.Charmain'),
+(32, 95, 'Carlito'),
+(33, 95, 'Charles'),
+(34, 96, 'Carlito'),
+(35, 96, 'Last'),
+(36, 96, 'Testing'),
+(37, 96, 'Sa'),
+(38, 96, 'Araw'),
+(39, 96, 'na'),
+(40, 96, 'to');
 
 -- --------------------------------------------------------
 
@@ -335,6 +392,19 @@ ALTER TABLE `swags`
   ADD PRIMARY KEY (`swag_id`);
 
 --
+-- Indexes for table `team`
+--
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`attendee_id`),
+  ADD KEY `fk_event_form_id` (`event_form_id`);
+
+--
+-- Indexes for table `team_history`
+--
+ALTER TABLE `team_history`
+  ADD PRIMARY KEY (`team_history_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -354,7 +424,7 @@ ALTER TABLE `brochures`
 -- AUTO_INCREMENT for table `event_form`
 --
 ALTER TABLE `event_form`
-  MODIFY `event_form_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=84;
+  MODIFY `event_form_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=97;
 
 --
 -- AUTO_INCREMENT for table `marketing_materials`
@@ -366,7 +436,7 @@ ALTER TABLE `marketing_materials`
 -- AUTO_INCREMENT for table `material_request_form`
 --
 ALTER TABLE `material_request_form`
-  MODIFY `material_request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=325;
+  MODIFY `material_request_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=335;
 
 --
 -- AUTO_INCREMENT for table `material_return_request`
@@ -379,6 +449,18 @@ ALTER TABLE `material_return_request`
 --
 ALTER TABLE `swags`
   MODIFY `swag_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `team`
+--
+ALTER TABLE `team`
+  MODIFY `attendee_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+
+--
+-- AUTO_INCREMENT for table `team_history`
+--
+ALTER TABLE `team_history`
+  MODIFY `team_history_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -402,6 +484,12 @@ ALTER TABLE `event_form`
 --
 ALTER TABLE `event_form_history`
   ADD CONSTRAINT `fk_event_form_history_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
+
+--
+-- Constraints for table `team`
+--
+ALTER TABLE `team`
+  ADD CONSTRAINT `fk_event_form_id` FOREIGN KEY (`event_form_id`) REFERENCES `event_form` (`event_form_id`) ON DELETE SET NULL ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
